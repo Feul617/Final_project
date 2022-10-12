27d79a85a4638f1146d1bce94ee25687fafc200f
Feul617 <ho0papa617@gmail.com>
2022-10-13 02:37:14 +0900

    프레임워크 추가
    타이틀 화면 이식
    캐릭터 이동 및 맵 파일 이식
    중력 개선(추가 수정 필요)

--- Bubble_Bobble.py
+++ Bubble_Bobble.py
@@ -1,66 +1,9 @@
 from pico2d import *
+import game_framework
+import title_state
 
 open_canvas()
-title = load_image('title.png')
-title_banner = load_image('insert_coin.png')
-map1 = load_image('stage1 Fairy_land map.png')
-map2 = load_image('stage2 dessert_land map.png')
-map3 = load_image('stage3 toy_land map.png')
-character = load_image('character2.png')
-
-
-class Monster:
-    def __init__(self):
-        self.x, self.y = 100, 50
-        self.frame
-
-    def update(self):
-        self.x += 2
-
-
-def handle_event():
-    global running
-    global start_title
-    global insert_coin
-    global character_x
-    global character_y
-    global dirx, diry
-
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            running = False
-        elif event.type == SDL_KEYDOWN:
-            if event.key == SDLK_RIGHT:
-                sight = 100
-                dirx += 1
-            elif event.key == SDLK_LEFT:
-                dirx -= 1
-                sight = 0
-            elif event.key == SDLK_SPACE:
-                for i in range(5):
-                    character_y += 10
-                    delay(0.01)
-            elif event.key == SDLK_ESCAPE:
-                running = False
-        elif event.type == SDL_KEYUP:
-            if event.key == SDLK_RIGHT:
-                dirx -= 1
-            elif event.key == SDLK_LEFT:
-                dirx += 1
-
-    pass
-
-
-def draw_title():
-    clear_canvas()
-    title.draw(400, 300)
-    if banner_on:
-        title_banner.draw(400, 30)
-
-    update_canvas()
-    pass
-
+game_framework.run(title_state)
 
 def draw_first_map():
     global character_x
@@ -106,38 +49,5 @@ def draw_character():
     elif character_x > 730:
         character_x -= 1
 
-
-#변수
-running = True
-banner_on = True
-start_title = False
-insert_coin = True
-first_step = True
-second_step = False
-stage = 5
-frame = 0
-character_x = 720
-character_y = 70
-dirx, diry = 0, 0;
-gravity = 0
-
-
-#메인문
-while running:
-    handle_event()
-    if start_title:
-        draw_title()
-        delay(0.8)
-        banner_on = not banner_on
-
-    if insert_coin:
-        if first_step:
-            draw_first_map()
-
-        if second_step:
-            draw_second_map()
-
-        draw_character()
-        update_canvas()
 
 close_canvas()
