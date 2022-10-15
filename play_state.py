d4f00e995080fb59b32bb332aa50b505465bd838
Feul617 <ho0papa617@gmail.com>
2022-10-15 16:24:00 +0900

    맵 class로 이동

--- play_state.py
+++ play_state.py
@@ -1,93 +1,95 @@
-from pico2d import *
+import pico2d
 import game_framework
 import title_state
 
 class Map:
-
     def __init__(self):
-        self.image = None
         self.step = 5
+        self.image = None
 
     def draw(self):
         self.image.clip_draw(0, self.step * 610, 800, 600, 400, 300)
 
     pass
 
-
 class Character:
     def __init__(self):
         self.x, self.y = 50, 50
         self.dir_x, self.dir_y = 0, 0
         self.gravity = 1
-        self.frame = None
-        self.image = load_image('./character/character2.png')
+        self.frame = 3
+        self.life = 3
+        self.image = pico2d.load_image('./character/character2.png')
 
     def draw(self):
-        self.image.clip_draw(self.frame * 50, 550, 60, 60, self.x, self.y - self.gravity)
+        self.image.clip_draw(self.frame * 50, 550, 70, 70, self.x, self.y)
 
-    def handle(self):
-        events = get_events()
-        for event in events:
-            if event.type == SDL_KEYDOWN:
-                if event.key == SDLK_RIGHT:
-                    self.dir_x += 1
-                elif event.key == SDLK_LEFT:
-                    self.dir_x -= 1
-                elif event.key == SDLK_SPACE:
-                    self.dir_y += 10
-
-
-
-
-    pass
-
-
-def handle_events():
-
-    events = get_events()
-    for event in events:
-        if event.type == SDL_QUIT:
-            game_framework.quit()
 
 def enter():
     global character, stage1, stage2, stage3
 
     character = Character()
-    stage1, stage2, stage3 = Map()
-    stage1.image = load_image('./map/stage1 Fairy_land map.png')
-    stage2.image = load_image('./map/stage2 dessert_land map.png')
+    stage1 = Map()
+    stage1.image = pico2d.load_image('./map/stage1 Fairy_land map.png')
     pass
 
 def exit():
-    global character, stage1, stage2, stage3
-    del character, stage1, stage2, stage3
     pass
 
-def map_draw():
-    global stage
+def handle_events():
+    events = pico2d.get_events()
+    for event in events:
+        if event.type == pico2d.SDL_QUIT:
+            game_framework.quit()
 
-    if stage == 1:
-        stage1.draw()
-    elif stage == 2:
-        stage2.draw()
-    elif stage == 3:
-        stage3.draw()
+        elif event.type == pico2d.SDL_KEYDOWN:
+            match event.key:
+                case pico2d.SDLK_ESCAPE:
+                    game_framework.quit()
+                #캐릭터 조종
+                case pico2d.SDLK_RIGHT:
+                    character.dir_x += 1
+                case pico2d.SDLK_LEFT:
+                    character.dir_x -= 1
+                case pico2d.SDLK_SPACE:
+                    character.y += 50
+
+        elif event.type == pico2d.SDL_KEYUP:
+            match event.key:
+                #캐릭터 조종
+                case pico2d.SDLK_RIGHT:
+                    character.dir_x -= 1
+                case pico2d.SDLK_LEFT:
+                    character.dir_x += 1
+
+def draw():
+    pico2d.clear_canvas()
+    stage1.draw()
+    character.draw()
+    pico2d.update_canvas()
     pass
 
-def character_move():
-    global character
+def update():
+    character.x += character.dir_x
+    character.y -= character.gravity
 
-    if 60 < character.x < 730:
-        character.x += character.dir_x * 3
-    elif character.x >= 730 or character.x <= 60:
-        character.x -= 1
+    if character.y < 0:
+        character.y = 600
+    pass
 
-running = True
-stage = 1
+def pause():
+    pass
 
-while running:
-    clear_canvas()
+def resume():
+    pass
 
-    character_move()
 
-    update_canvas()
+def test_self():
+    import sys
+    this_module = sys.modules['__main__']
+    pico2d.open_canvas()
+    game_framework.run(this_module)
+    pico2d.close_canvas()
+
+if __name__ == '__main__': #만약 단독 실행 상태이면
+    test_self()
