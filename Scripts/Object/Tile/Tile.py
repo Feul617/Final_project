58ab5b6e86ee8dfdbc1475514fe8a043df8e29f3
Feul617 <ho0papa617@gmail.com>
2022-11-03 03:37:33 +0900

    발판 그림 추가 및 리팩토링

--- Scripts/Object/Tile/Tile.py
+++ Scripts/Object/Tile/Tile.py
@@ -19,13 +19,13 @@ class Tile(Object):
 def TileType(index):
     tile = Tile()
     if index == 0:
-        tile.image = load_image('./map/Tile/stage1-1 scaffolding_left.png')
+        tile.image = load_image('./map/Tile/stage1-1 left_tile.png')
         tile.image_Type = [0, 0, 18, 32]
     elif index == 1:
-        tile.image = load_image('map/Tile/stage1-1 scaffolding_middle.png')
+        tile.image = load_image('map/Tile/stage1-1 middle_tile.png')
         tile.image_Type = [0, 0, 36, 32]
     elif index == 2:
-        tile.image = load_image('map/Tile/stage1-1 scaffolding_right.png')
+        tile.image = load_image('map/Tile/stage1-1 right_tile.png')
         tile.image_Type = [0, 0, 18, 32]
 
     return tile
--- Scripts/Stage/MainStage.py
+++ Scripts/Stage/MainStage.py
@@ -4,21 +4,41 @@ from Scripts.Object.Object_AFX import *
 class MainStage:
     def __init__(self):
         self.background = Object()
-        self.background.image = load_image('./map/stage1 Fairy_land map_temp.png')
+        self.background.image = load_image('./map/stage1 Fairy_land map.png')
         self.background.transform.position = Vector2(800//2, 600//2)
         self.frame = 5
-        self.background.image_Type = [0, self.frame * 590, 800, 600]
+        self.background.image_Type = [0, self.frame * 610, 800, 600]
         self.tiles = []
 
 
     def Tile_init(self):
         #1층 왼쪾 타일
-        tile = MakeTile_X(10)
-        tile.MakeTile(50, 50)
+        tile = MakeTile_X(7)
+        tile.MakeTile(165, 133)
         self.tiles.append(tile)
 
         #1층 오른쪽 타일
-        tile = MakeTile_X(10)
-        tile.MakeTile(400, 50)
+        tile = MakeTile_X(7)
+        tile.MakeTile(450, 133)
+        self.tiles.append(tile)
+
+        #2층 왼쪽 타일
+        tile = MakeTile_X(7)
+        tile.MakeTile(120, 240)
+        self.tiles.append(tile)
+
+        #2층 오른쪽 타일
+        tile = MakeTile_X(7)
+        tile.MakeTile(500, 240)
+        self.tiles.append(tile)
+
+        #3층 왼쪽 타일
+        tile = MakeTile_X(6)
+        tile.MakeTile(200, 345)
+        self.tiles.append(tile)
+
+        #3층 오른쪽 타일
+        tile = MakeTile_X(6)
+        tile.MakeTile(450, 345)
         self.tiles.append(tile)
     pass
