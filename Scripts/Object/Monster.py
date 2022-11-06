5535f1057e45b48ec778aa2783e583e3e10a81e9
Feul617 <ho0papa617@gmail.com>
2022-11-06 21:10:36 +0900

    몬스터 오브젝트 추가
    수정 많이 필요

---
+++ Scripts/Object/Monster.py
@@ -0,0 +1,19 @@
+from Scripts.FrameWork.FrameWork_AFX import *
+
+class Monster:
+    def __init__(self):
+        super(Monster, self).__init__()
+        self.dir_x, self.dir_y = 0, 0
+        self.image = load_image('./character/monster2.png')
+        self.type = 0
+
+    def get_bb(self):
+        return self.x - 20, self.y, self.x + 20, self.y + 60
+
+def Monster_Type(index):
+    monster = Monster()
+
+    if index == 0:
+        monster.image_type = [0, 500, 52, 52]
+    elif index == 1:
+        monster.image_type = [0, 450, 52, 52]
