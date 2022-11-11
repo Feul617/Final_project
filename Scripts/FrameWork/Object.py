45d133bfbbff6e11d9d492f3d480b273934d9394
Feul617 <ho0papa617@gmail.com>
2022-11-11 18:14:33 +0900

    그림 방향 추가
    좌우 그림 추가

--- Scripts/FrameWork/Object.py
+++ Scripts/FrameWork/Object.py
@@ -5,6 +5,7 @@ class Object:
     def __init__(self):
         self.image = None
         self.image_Type = None # frame
+        self.flip = ' '
 
         self.transform = Transform()
         pass
@@ -15,8 +16,11 @@ class Object:
     def Draw(self):
         pos = self.transform.position
         scale = self.transform.scale
-        self.image.clip_draw(self.image_Type[0], self.image_Type[1], self.image_Type[2], self.image_Type[3], pos.x, pos.y,
+        self.image.clip_composite_draw(self.image_Type[0], self.image_Type[1], self.image_Type[2], self.image_Type[3], 0, self.flip, pos.x, pos.y,
                              scale.x * self.image_Type[2], scale.y * self.image_Type[3])
 
+        pass
+
+    def update(self):
         pass
     pass
