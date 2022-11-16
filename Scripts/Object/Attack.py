9df470a7986ea216b86ed7bab6408f5f18fa09cc
Feul617 <ho0papa617@gmail.com>
2022-11-16 23:49:43 +0900

    공격 변경
    상승하는 버블 이미지 추가
    맵에서 벗어날 시 버블 삭제

--- Scripts/Object/Attack.py
+++ Scripts/Object/Attack.py
@@ -6,6 +6,7 @@ SHOOT_SPEED_KMPH = 300.0 # km/h 마라토너의 평속
 SHOOT_SPEED_MPM = SHOOT_SPEED_KMPH * 1000 / 60.0
 SHOOT_SPEED_MPS = SHOOT_SPEED_MPM / 60.0
 SHOOT_SPEED_PPS = SHOOT_SPEED_MPS * PIXEL_PER_METER
+UP_SPEED_PPS = 10.0 * 1000 / 60.0 / 60.0 * PIXEL_PER_METER
 
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
@@ -15,7 +16,6 @@ class Bubble(Object):
     def __init__(self, x = 400, y = 300, velocity = 1):
         super(Bubble, self).__init__()
         self.image = load_image('./character/attack.png')
-        self.image2 = None
         self.transform.position.x,\
         self.transform.position.y, \
         self.velocity = x, y, velocity
@@ -27,10 +27,19 @@ class Bubble(Object):
         self.transform.scale.x = 2
 
 
-
     def update(self):
         self.image_Type = [int(self.frame) * 17, 0, 15, 50]
         self.transform.position.x += SHOOT_SPEED_KMPH * game_framework.frame_time * self.velocity
+
+        if self.transform.position.x - self.start_x > 150:
+            if self.velocity != 0:
+                self.image = load_image('./character/bubble.png')
+            self.velocity = 0
+
+            self.transform.position.y += UP_SPEED_PPS * game_framework.frame_time
+            self.frame = (self.frame + FRAME_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 3.0
+
+            pass
 
         if abs(self.transform.position.x - self.start_x) >= 80:
             pass
@@ -38,7 +47,7 @@ class Bubble(Object):
         self.frame = (self.frame + FRAME_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 6.0
 
         self.transform.position.x = clamp(80, self.transform.position.x, 720)
-        if self.transform.position.x == 80 or self.transform.position.x == 720:
+        if self.transform.position.x == 80 or self.transform.position.x == 720 or self.transform.position.y > 820:
             remove_object(self)
 
     def get_bb(self):
