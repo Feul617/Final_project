f47ff42cc13dab04c5ad0237e5d0f00c7699be41
Feul617 <ho0papa617@gmail.com>
2022-11-17 16:09:58 +0900

    방울과 충돌처리
    부딪히면 방울에 갇힌 몬스터로 변경 상승은 유지

--- Scripts/Object/Monster.py
+++ Scripts/Object/Monster.py
@@ -14,14 +14,17 @@ class Monster(Object):
     def __init__(self):
         super(Monster, self).__init__()
         self.frame = 0
+        self.frame_set = 4.0
         self.dir = 1
         self.flip = ' '
         self.name = ' '
         self.image = load_image('./character/monster2.png')
         self.image_Type = [0, 0, 0, 0]
 
-        self.state = None
+        self.state = 0 # 0:init / 1:in bubble
+        self.type = 0
         self.count = 0
+        self.in_bubble = 0
 
     def update(self):
         self.face_dir = self.dir
@@ -29,10 +32,10 @@ class Monster(Object):
             self.flip = ' '
         else:
             self.flip = 'h'
-        self.frame = (self.frame + FRAME_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4.0
+        self.frame = (self.frame + FRAME_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % self.frame_set
         self.transform.position.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time
-        if self.name == 'zen_chan':
-            self.image_Type = [int(self.frame) * 55, 800, 40, 60]
+        self.Monster_type()
+        self.image_Type = [(int(self.frame) + self.in_bubble) * 55, self.type, 40, 60]
 
     def get_bb(self):
         return self.transform.position.x - 20, self.transform.position.y - 30, \
@@ -44,14 +47,18 @@ class Monster(Object):
             print('캐릭터충돌')
             self.count -= 1
             remove_object(self)
+        elif group == 'attack:monster':
+            if self.name == 'zen_chan':
+                self.in_bubble = 16
+                self.frame_set = 3.0
 
-def Monster_Type(index):
-    monster = Monster()
+    def Monster_type(self):
+        match self.name:
+            case 'zen_chan':
+                self.type = 800
 
-    if index == 0:
-        monster.image_type = [0, 500, 52, 52]
-    elif index == 1:
-        monster.image_type = [0, 450, 52, 52]
+            case 'mighta':
+                self.type = 700
 
 
 class Make_Monster(Object):
