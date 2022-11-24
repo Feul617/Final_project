e6e5b7cf64f9516cf522a9db2da2050cb99ec2f6
Feul617 <ho0papa617@gmail.com>
2022-11-24 20:35:48 +0900

    스테이지 분할

--- Scripts/Stage/Stage1_1.py
+++ Scripts/Stage/Stage1_1.py
@@ -7,18 +7,37 @@ class Stage1_1(MainStage):
         self.background.image = load_image('./map/stage1 Fairy_land map_temp.png')
         self.background.transform.position = Vector2(800 // 2, 600 // 2)
         self.frame = 610
-        self.stage = 1
+        self.stage = 2
         self.background.image_Type = [0, self.stage * self.frame, 800, 600]
         self.tiles = []
+        self.frame_move_time = 0
+        self.is_Next = False
+
+
+        Object.gameWorld.add_object(self.background, 0)
+        Object.gameWorld.add_object(self, 0)
 
     def update(self):
         self.background.image_Type = [0, self.stage * self.frame, 800, 600]
+        if MainStage.monster_count == 0 and self.frame_move_time >= 100:
+            self.frame += 10
+
+        self.frame_move_time += 1
+        if self.frame_move_time >= 100:
+            self.frame_move_time = 0
+
+        if Monster.monster_count == 0:
+            self.is_Next = True
+            Monster.monster_count = 4
+            self.nextStage.isActive = True
+
+        if self.is_Next:
+            Camera.mainCamera.transform.position.y += game_framework.frame_time
 
     def handle_events(self, event):
-        if event.type == SDL_KEYDOWN and event.key == SDLK_a:
-            self.frame -= 600
+        pass
 
-    def init(self):
+    def Init(self):
         # 공용 타일
         tile = MakeTile_X(6, self.stage)
         tile.MakeTile(62, 28)
@@ -45,7 +64,7 @@ class Stage1_1(MainStage):
         # 2층 왼쪽 타일
         tile = MakeTile_X(7, self.stage)
         tile.MakeTile(120, 240)
-        self.zen_chan[0].transform.position = Vector2(120, 240)
+        self.zen_chan[0].transform.position = Vector2(120, 280)
         self.zen_chan[0].patrolPos = Vector2(130, 305)
 
         self.tiles.append(tile)
@@ -53,21 +72,21 @@ class Stage1_1(MainStage):
         # 2층 오른쪽 타일
         tile = MakeTile_X(7, self.stage)
         tile.MakeTile(490, 240)
-        self.zen_chan[1].transform.position = Vector2(490, 240)
+        self.zen_chan[1].transform.position = Vector2(490, 280)
         self.zen_chan[1].patrolPos = Vector2(500, 675)
         self.tiles.append(tile)
 
         # 3층 왼쪽 타일
         tile = MakeTile_X(6, self.stage)
         tile.MakeTile(200, 345)
-        self.zen_chan[2].transform.position = Vector2(200, 345)
+        self.zen_chan[2].transform.position = Vector2(200, 380)
         self.zen_chan[2].patrolPos = Vector2(215, 370)
         self.tiles.append(tile)
 
         # 3층 오른쪽 타일
         tile = MakeTile_X(6, self.stage)
         tile.MakeTile(450, 345)
-        self.zen_chan[3].transform.position = Vector2(450, 345)
+        self.zen_chan[3].transform.position = Vector2(450, 380)
         self.zen_chan[3].patrolPos = Vector2(460, 615)
         self.tiles.append(tile)
 
--- Scripts/Stage/Stage1_2.py
+++ Scripts/Stage/Stage1_2.py
@@ -1,25 +1,33 @@
-from Scripts.Object.Object_AFX import *
-from Scripts.Stage.MainStage import MainStage
+
+from Scripts.Stage.MainStage import *
+
 
 class Stage1_2(MainStage):
+
     def __init__(self):
-        super(self.stage1_2, self).__init__()
+        super(Stage1_2, self).__init__()
         self.background = Object()
-        self.background.image = load_image('./map/self.stage1 Fairy_land map_temp.png')
-        self.background.transform.position = Vector2(800 // 2, 600 // 2)
+        self.background.image = load_image('./map/stage1 Fairy_land map_temp.png')
+        self.background.transform.position = Vector2(800 // 2, 600 // 2 - 600)
         self.frame = 610
-        self.stage = 2
-        self.background.image_Type = [0, self.stage * self.frame, 800, 600]
+        self.stage = 1
+        self.background.image_Type = [0, (6-self.stage) * self.frame, 800, 600]
         self.tiles = []
+        self.isActive = True
+
+        Camera.mainCamera.transform.position.y = -100
+
+        Object.gameWorld.add_object(self.background, 0)
+        Object.gameWorld.add_object(self, 0)
+
 
     def update(self):
         self.background.image_Type = [0, self.stage * self.frame, 800, 600]
 
     def handle_events(self, event):
-        if event.type == SDL_KEYDOWN and event.key == SDLK_a:
-            self.frame -= 600
+        pass
 
-    def init(self):
+    def Init(self):
         # 공용 타일
         tile = MakeTile_X(6, self.stage)
         tile.MakeTile(62, 28)
