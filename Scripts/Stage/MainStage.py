0a4badca4c1abf80f8c90fefc754a8310835286d
Feul617 <ho0papa617@gmail.com>
2022-11-24 20:35:41 +0900

    부모 클래스로 변경
    mainstage를 부모 클래스로 두고 stage별로 자식을 만들어서 이미지와 좌표 분할

--- Scripts/Stage/MainStage.py
+++ Scripts/Stage/MainStage.py
@@ -1,8 +1,11 @@
 from Scripts.Object.Object_AFX import *
 from Scripts.Object.Monster import Monster
+from Scripts.FrameWork.Camera import Camera
 
 class MainStage(Object):
+    monster_count = 0
     def __init__(self):
+        super(MainStage, self).__init__()
         # self.background = Object()
         # self.background.image = load_image('./map/stage1 Fairy_land map_temp.png')
         # self.background.transform.position = Vector2(800//2, 600//2)
@@ -10,6 +13,7 @@ class MainStage(Object):
         # self.stage = 5
         # self.background.image_Type = [0, self.stage * self.frame, 800, 600]
         # self.tiles = []
+        self.nextStage = None
 
         #몬스터
         self.zen_chan = [Monster() for i in range(4)]
@@ -20,6 +24,8 @@ class MainStage(Object):
     def update(self):
         self.background.image_Type = [0, self.stage * self.frame, 800, 600]
 
+    def Draw(self):
+        pass
     def handle_events(self, event):
         if event.type == SDL_KEYDOWN and event.key == SDLK_a:
             self.frame -= 600
