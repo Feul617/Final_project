317a5e5b3ac286733283c1a8842f34ff436b2935
Feul617 <ho0papa617@gmail.com>
2022-11-26 15:22:39 +0900

    타일 위치 조정 및 생성

--- Scripts/Stage/Stage1_2.py
+++ Scripts/Stage/Stage1_2.py
@@ -10,61 +10,71 @@ class Stage1_2(MainStage):
         self.isActive = True
 
         # background 초기화
-        self.background.image = load_image('./map/Background/stage1-2 bg.png')
-        self.background.transform.position = Vector2(800 // 2, 600 // 2 - (self.height * (self.stage - 1)))
-        self.background.image_Type = [0, 0, 800, 600]
+        #self.background.image = load_image('./map/Background/stage1-2 bg.png')
+        self.background.image = load_image('./map/stage1 Fairy_land map.png')
+        self.background.transform.position = Vector2(800 // 2, 610 // 2 - (self.height * (self.stage - 1)))
+        self.background.image_Type = [0, 610 * 4, 800, 610]
 
-        Camera.mainCamera.transform.position.y -= 600
+        Camera.mainCamera.transform.position.y -= 610
 
     def handle_events(self, event):
         pass
 
     def Init(self):
-        if self.isActive:
-            # 공용 타일
-            tile = MakeTile_X(6, self.stage)
-            tile.MakeTile(62, 33 - self.height)
-            self.tiles.append(tile)
+        # 공용 타일
+        tile = MakeTile_X(6, self.stage)
+        tile.MakeTile(62, 30 - self.height)
+        self.tiles.append(tile)
 
-            tile = MakeTile_X(6, self.stage)
-            tile.MakeTile(330, 33)
-            self.tiles.append(tile)
+        tile = MakeTile_X(6, self.stage)
+        tile.MakeTile(330, 30 - self.height)
+        self.tiles.append(tile)
 
-            tile = MakeTile_X(6, self.stage)
-            tile.MakeTile(580, 33)
-            self.tiles.append(tile)
+        tile = MakeTile_X(6, self.stage)
+        tile.MakeTile(580, 30 - self.height)
+        self.tiles.append(tile)
 
-            # 1층 왼쪽 타일
-            tile = MakeTile_X(7, self.stage)
-            tile.MakeTile(165, 133)
-            self.tiles.append(tile)
+        # 1층 왼쪽 타일
+        tile = MakeTile_X(6, self.stage)
+        tile.MakeTile(120, 133 - self.height)
+        self.tiles.append(tile)
 
-            # 1층 오른쪽 타일
-            tile = MakeTile_X(7, self.stage)
-            tile.MakeTile(450, 133)
-            self.tiles.append(tile)
+        # 1층 중앙 타일
+        tile = MakeTile_X(3, self.stage)
+        tile.MakeTile(380, 133 - self.height)
+        self.tiles.append(tile)
 
-            # 2층 왼쪽 타일
-            tile = MakeTile_X(7, self.stage)
-            tile.MakeTile(120, 240)
-            self.tiles.append(tile)
+        # 1층 오른쪽 타일
+        tile = MakeTile_X(6, self.stage)
+        tile.MakeTile(525, 133 - self.height)
+        self.tiles.append(tile)
 
-            # 2층 오른쪽 타일
-            tile = MakeTile_X(7, self.stage)
-            tile.MakeTile(490, 240)
-            self.tiles.append(tile)
+        # 2층 왼쪽 타일
+        tile = MakeTile_X(8, self.stage)
+        tile.MakeTile(140, 240 - self.height)
+        self.tiles.append(tile)
 
-            # 3층 왼쪽 타일
-            tile = MakeTile_X(6, self.stage)
-            tile.MakeTile(200, 345)
-            self.tiles.append(tile)
+        # 2층 오른쪽 타일
+        tile = MakeTile_X(8, self.stage)
+        tile.MakeTile(450, 240 - self.height)
+        self.tiles.append(tile)
 
-            # 3층 오른쪽 타일
-            tile = MakeTile_X(6, self.stage)
-            tile.MakeTile(450, 345)
-            self.tiles.append(tile)
+        # 3층 왼쪽 타일
+        tile = MakeTile_X(8, self.stage)
+        tile.MakeTile(140, 345 - self.height)
+        self.tiles.append(tile)
 
-            # 4층 왼쪽 타일
-            tile = MakeTile_X(6, self.stage)
-            tile.MakeTile(200, 450)
-            self.tiles.append(tile)
+        # 3층 오른쪽 타일
+        tile = MakeTile_X(8, self.stage)
+        tile.MakeTile(450, 345 - self.height)
+        self.tiles.append(tile)
+
+        # 4층 왼쪽 타일
+        tile = MakeTile_X(8, self.stage)
+        tile.MakeTile(140, 450 - self.height)
+        self.tiles.append(tile)
+
+        # 4층 오른쪽 타일
+        tile = MakeTile_X(8, self.stage)
+        tile.MakeTile(450, 450 - self.height)
+        self.tiles.append(tile)
