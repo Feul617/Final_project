b56650d667a11646eb71c7bafd5ea5f80c4c310e
Feul617 <ho0papa617@gmail.com>
2022-11-26 16:33:20 +0900

    1-2스테이지 타일 완성

--- Scripts/Stage/Stage1_2.py
+++ Scripts/Stage/Stage1_2.py
@@ -7,7 +7,7 @@ class Stage1_2(MainStage):
     def __init__(self):
         super(Stage1_2, self).__init__()
         self.stage = 2
-        self.isActive = True
+        self.isActive = False
 
         # background 초기화
         #self.background.image = load_image('./map/Background/stage1-2 bg.png')
@@ -23,58 +23,58 @@ class Stage1_2(MainStage):
     def Init(self):
         # 공용 타일
         tile = MakeTile_X(6, self.stage)
-        tile.MakeTile(62, 30 - self.height)
+        tile.MakeTile(62, 30 - (self.height * (self.stage - 1)))
         self.tiles.append(tile)
 
         tile = MakeTile_X(6, self.stage)
-        tile.MakeTile(330, 30 - self.height)
+        tile.MakeTile(330, 30 - (self.height * (self.stage - 1)))
         self.tiles.append(tile)
 
         tile = MakeTile_X(6, self.stage)
-        tile.MakeTile(580, 30 - self.height)
+        tile.MakeTile(580, 30 - (self.height * (self.stage - 1)))
         self.tiles.append(tile)
 
         # 1층 왼쪽 타일
         tile = MakeTile_X(6, self.stage)
-        tile.MakeTile(120, 133 - self.height)
+        tile.MakeTile(120, 133 - (self.height * (self.stage - 1)))
         self.tiles.append(tile)
 
         # 1층 중앙 타일
         tile = MakeTile_X(3, self.stage)
-        tile.MakeTile(380, 133 - self.height)
+        tile.MakeTile(380, 133 - (self.height * (self.stage - 1)))
         self.tiles.append(tile)
 
         # 1층 오른쪽 타일
         tile = MakeTile_X(6, self.stage)
-        tile.MakeTile(525, 133 - self.height)
+        tile.MakeTile(525, 133 - (self.height * (self.stage - 1)))
         self.tiles.append(tile)
 
         # 2층 왼쪽 타일
         tile = MakeTile_X(8, self.stage)
-        tile.MakeTile(140, 240 - self.height)
+        tile.MakeTile(140, 240 - (self.height * (self.stage - 1)))
         self.tiles.append(tile)
 
         # 2층 오른쪽 타일
         tile = MakeTile_X(8, self.stage)
-        tile.MakeTile(450, 240 - self.height)
+        tile.MakeTile(450, 240 - (self.height * (self.stage - 1)))
         self.tiles.append(tile)
 
         # 3층 왼쪽 타일
         tile = MakeTile_X(8, self.stage)
-        tile.MakeTile(140, 345 - self.height)
+        tile.MakeTile(140, 345 - (self.height * (self.stage - 1)))
         self.tiles.append(tile)
 
         # 3층 오른쪽 타일
         tile = MakeTile_X(8, self.stage)
-        tile.MakeTile(450, 345 - self.height)
+        tile.MakeTile(450, 345 - (self.height * (self.stage - 1)))
         self.tiles.append(tile)
 
         # 4층 왼쪽 타일
         tile = MakeTile_X(8, self.stage)
-        tile.MakeTile(140, 450 - self.height)
+        tile.MakeTile(140, 450 - (self.height * (self.stage - 1)))
         self.tiles.append(tile)
 
         # 4층 오른쪽 타일
         tile = MakeTile_X(8, self.stage)
-        tile.MakeTile(450, 450 - self.height)
+        tile.MakeTile(450, 450 - (self.height * (self.stage - 1)))
         self.tiles.append(tile)
