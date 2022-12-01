4fd4201895133f5b1c20c4302d4e32c1a2e728a0
Feul617 <ho0papa617@gmail.com>
2022-12-02 02:36:00 +0900

    몬스터 담아두는 server

---
+++ Scripts/Object/Monster/MonsterContain.py
@@ -0,0 +1,4 @@
+from Scripts.Object.Monster.Zen_chan import Zen_chan
+from Scripts.Object.Monster.Monsta import Monsta
+from Scripts.Object.Monster.Mighta import Mighta
+from Scripts.Object.Monster.Boss import Boss
