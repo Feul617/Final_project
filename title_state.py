4e1b659d04ed8285be8053083bb6714a9a9dd8e6
Feul617 <ho0papa617@gmail.com>
2022-12-03 18:16:11 +0900

    시작 타이틀 프레임 드랍 해결

--- title_state.py
+++ title_state.py
@@ -2,16 +2,22 @@ from pico2d import *
 import game_framework
 import play_state
 
+next_stage = None
+banner_delay = None
+
 def enter():
-    global image, banner, banner_on, banner_time, count, bgm, bgm_on
+    global image, banner, banner_on, banner_time
+    global count, bgm, bgm_on, next_stage, banner_delay
     image = load_image('./map/title.png')
     banner = load_image('./map/insert_coin.png')
     bgm = load_music('./sound/Invincible(start screen) .mp3')
     bgm.set_volume(10)
     bgm_on = True
     banner_on = True
-    banner_time = 0.5
+    banner_time = 200
+    banner_delay = 1
     count = 0
+    next_stage = False
     pass
 
 
@@ -21,7 +27,7 @@ def exit():
     pass
 
 def handle_events():
-    global banner_time, count
+    global banner_time, count, next_stage, banner_delay
 
     events = get_events()
 
@@ -31,7 +37,9 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
-            banner_time = 0.1
+            next_stage = True
+            banner_time = 1
+            banner_delay = 4
 
     pass
 
@@ -45,22 +53,31 @@ def draw():
     update_canvas()
 
 def update():
-    global banner_on, count, bgm, bgm_on
+    global banner_on, count, bgm, bgm_on, banner_time, next_stage, banner_delay
 
-    delay(banner_time)
-    banner_on = not banner_on
+    banner_time -= 1
+    if banner_time == 0:
+        banner_time = 200 / banner_delay
+        banner_on = not banner_on
+
     if bgm_on:
         bgm.play(1)
         bgm_on = False
-    count += 1
-    if count >= 20:
-        game_framework.change_state(play_state)
+
+    if next_stage:
+        count += 1
+        if count == 700:
+            game_framework.change_state(play_state)
 
 def pause():
     pass
 
 def resume():
     pass
+
+def Banner_on():
+    global banner_on
+    banner_on = not banner_on
 
 def test_self():
     import sys
